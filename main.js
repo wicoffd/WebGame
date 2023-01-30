8fa620d98bcdd2839a931ed8befc15c2be005597
wicoffd <98190530+wicoffd@users.noreply.github.com>
2023-01-29 19:57:10 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ ASSET_MANAGER.queueDownload("./Mouse.png")
 ASSET_MANAGER.queueDownload("./Chicken.png")
 ASSET_MANAGER.queueDownload("./Owl.png")
 ASSET_MANAGER.queueDownload("./hero.png")
+ASSET_MANAGER.queueDownload("./doggy.png")
 ASSET_MANAGER.queueDownload("./MapHouse.png")
+
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
@@ -17,8 +19,16 @@ ASSET_MANAGER.downloadAll(() => {
 	background.src = "./MapHouse.png";
 	
 	gameEngine.addEntity(new Mouse(gameEngine,"3","left",250,420));
-	gameEngine.addEntity(new Chicken(gameEngine,"1","left",910,500));
+	gameEngine.addEntity(new Mouse(gameEngine,"7","left",270,410));
+	gameEngine.addEntity(new Mouse(gameEngine,"8","left",230,400));
+	gameEngine.addEntity(new Chicken(gameEngine,"1","left",890,450));
+	gameEngine.addEntity(new Chicken(gameEngine,"3","left",910,500));
+	gameEngine.addEntity(new Chicken(gameEngine,"8","left",920,470));
 	gameEngine.addEntity(new Owl(gameEngine,"4","left",500,450));
+	gameEngine.addEntity(new Owl(gameEngine,"6","left",430,450));
+	gameEngine.addEntity(new Owl(gameEngine,"8","left",450,450));
+	gameEngine.addEntity(new Owl(gameEngine,"1","left",460,410));
+	gameEngine.addEntity(new LittleDog(gameEngine));
 	gameEngine.addEntity(new Player(gameEngine,"1","",700,600));
 	gameEngine.init(ctx);
 
